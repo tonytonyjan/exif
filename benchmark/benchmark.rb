b7f9a9381bd10042aa1412f6eab7ff9677b8bd0a
Jian Weihang <tonytonyjan@gmail.com>
2014-10-30 16:15:01 +0800

    update benchmark

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -5,19 +5,11 @@ require 'exifr'
 
 N = 500
 FILE_PATH = File.expand_path('../../spec/sample.jpg', __FILE__)
-exifr = EXIFR::JPEG.new(FILE_PATH)
-exif  = Exif::Data.new(FILE_PATH)
 Benchmark.bmbm do |x|
   x.report '[exifr] init' do
     N.times{ EXIFR::JPEG.new(FILE_PATH).width }
   end
   x.report '[exif]  init' do
     N.times{ Exif::Data.new(FILE_PATH).image_width }
-  end
-  x.report '[exifr] get width' do
-    N.times{ exifr.width }
-  end
-  x.report '[exif]  get width' do
-    N.times{ exif.image_width }
   end
 end
