e67405bdf57d20552ff99e1ed5247161e87f42ab
Jian Weihang <tonytonyjan@gmail.com>
2015-08-03 00:22:33 +0800

    code refactoring.

--- ext/data.c
+++ ext/data.c
@@ -3,7 +3,7 @@
 #include "data.h"
 
 extern VALUE rb_mExif, rb_eNotReadble, rb_eIFDNotFound, rb_eUnknownDataType;
-extern const char* exif_entry_to_ivar(ExifEntry* ee);
+extern const char* exif_entry_to_ivar(ExifEntry* entry);
 
 VALUE rb_cData;
 
@@ -51,14 +51,14 @@ void each_content(ExifContent *ec, void *self_ptr){
   exif_content_foreach_entry(ec, each_entry, self);
 }
 
-void each_entry(ExifEntry *ee, void *self_ptr){
-  exif_entry_dump(ee, 2);
+void each_entry(ExifEntry *entry, void *self_ptr){
+  exif_entry_dump(entry, 2);
   VALUE *self, value;
   const char *ivar_name;
 
   self = (VALUE*)self_ptr;
-  ivar_name = exif_entry_to_ivar(ee);
-  value = exif_entry_to_value(ee);
+  ivar_name = exif_entry_to_ivar(entry);
+  value = exif_entry_to_value(entry);
 
   rb_iv_set(*self, ivar_name, value);
 }
@@ -66,48 +66,85 @@ void each_entry(ExifEntry *ee, void *self_ptr){
 VALUE exif_entry_to_value(ExifEntry *entry){
   ExifData  *data;
   ExifByteOrder order;
+  ExifRational rational;
+  ExifSRational srational;
   VALUE ret;
   int i;
+  unsigned char size;
 
   data = entry->parent->parent;
   order = exif_data_get_byte_order(data);
   ret = Qnil;
+  size = exif_format_get_size(entry->format);
 
   switch(entry->format){
-  case EXIF_FORMAT_BYTE:
-    break;
   case EXIF_FORMAT_ASCII:
     ret = rb_str_new2((const char *)entry->data);
     break;
   case EXIF_FORMAT_SHORT:
     if(entry->components > 1){
       ret = rb_ary_new2(entry->components);
-      for(i = 0; i < entry->components; i++)
-        rb_ary_push(ret, INT2FIX(exif_get_short(entry->data + exif_format_get_size(entry->format) * i, order)));
+      for(int i = 0; i < entry->components; i++)
+        rb_ary_push(ret, INT2FIX(exif_get_short(entry->data + i*size, order)));
     }else ret = INT2FIX(exif_get_short(entry->data, order));
-    break;
-  case EXIF_FORMAT_LONG:
-    break;
-  case EXIF_FORMAT_RATIONAL:
-    break;
-  case EXIF_FORMAT_SBYTE:
-    break;
-  case EXIF_FORMAT_UNDEFINED:
     break;
   case EXIF_FORMAT_SSHORT:
     if(entry->components > 1){
       ret = rb_ary_new2(entry->components);
-      for(i = 0; i < entry->components; i++)
-        rb_ary_push(ret, INT2FIX(exif_get_sshort(entry->data + exif_format_get_size(entry->format) * i, order)));
+      for(int i = 0; i < entry->components; i++)
+        rb_ary_push(ret, INT2FIX(exif_get_sshort(entry->data + i*size, order)));
     }else ret = INT2FIX(exif_get_sshort(entry->data, order));
     break;
-  case EXIF_FORMAT_SLONG:
+  case EXIF_FORMAT_RATIONAL:
+    if(entry->components > 1){
+      ret = rb_ary_new2(entry->components);
+      for(int i = 0; i < entry->components; i++){
+        srational = exif_get_srational(entry->data + i*size, order);
+        rb_ary_push(ret, rb_rational_new(LONG2FIX(srational.numerator), LONG2FIX(srational.denominator)));
+      }
+    }else{
+      srational = exif_get_srational(entry->data, order);
+      ret = rb_rational_new(LONG2FIX(srational.numerator), LONG2FIX(srational.denominator));
+    }
     break;
   case EXIF_FORMAT_SRATIONAL:
+    if(entry->components > 1){
+      ret = rb_ary_new2(entry->components);
+      for(int i = 0; i < entry->components; i++){
+        rational = exif_get_rational(entry->data + i*size, order);
+        rb_ary_push(ret, rb_rational_new(LONG2FIX(rational.numerator), LONG2FIX(rational.denominator)));
+      }
+    }else{
+      rational = exif_get_rational(entry->data, order);
+      ret = rb_rational_new(LONG2FIX(rational.numerator), LONG2FIX(rational.denominator));
+    }
+    break;
+  case EXIF_FORMAT_BYTE:
+  case EXIF_FORMAT_SBYTE:
+    ret = rb_str_new((const char *)entry->data, entry->size);
+    break;
+  case EXIF_FORMAT_LONG:
+    if(entry->components > 1){
+      ret = rb_ary_new2(entry->components);
+      for(int i = 0; i < entry->components; i++)
+        rb_ary_push(ret, LONG2NUM(exif_get_long(entry->data + i*size, order)));
+    }else ret = LONG2NUM(exif_get_long(entry->data, order));
+    break;
+  case EXIF_FORMAT_SLONG:
+    if(entry->components > 1){
+      ret = rb_ary_new2(entry->components);
+      for(int i = 0; i < entry->components; i++)
+        rb_ary_push(ret, LONG2NUM(exif_get_slong(entry->data + i*size, order)));
+    }else ret = LONG2NUM(exif_get_slong(entry->data, order));
     break;
   case EXIF_FORMAT_FLOAT:
+    // TODO
     break;
   case EXIF_FORMAT_DOUBLE:
+    // TODO
+    break;
+  case EXIF_FORMAT_UNDEFINED:
+    // TODO
     break;
   }
 
--- spec/exif_spec.rb
+++ spec/exif_spec.rb
@@ -6,8 +6,13 @@ describe Exif do
     expect(data.model).to eq 'NIKON D600'
     expect(data.image_width).to eq 4000
     expect(data.bits_per_sample).to eq [8, 8, 8]
-    # expect(data.gps_latitude).to be_within(0.0001).of(24.178028333333334)
-    # expect(data.date_time).to eq Time.new(2013,12,8,21,14,11)
+    expect(data.gps_latitude).to eq [Rational(24, 1), Rational(106817, 10000), Rational(0, 1)]
+    expect(data.gps_time_stamp).to eq [Rational(8, 1), Rational(4, 1), Rational(25, 1)]
+    expect(data.gps_altitude).to eq Rational(332, 1)
+    expect(data.pixel_x_dimension).to eq 4000
+    expect(data.gps_version_id).to eq "\x02\x02\x00\x00"
+    expect(data.gps_altitude_ref).to eq "\x00"
+    # expect(data.date_time).to eq Time.new(2013,12,8,21,14,11) TODO
   end
 
   it 'raises errors' do
